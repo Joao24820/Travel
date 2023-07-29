cd722a3aec46f9f8e919ad7662a118163623a398
João Vitor <71740709+Joao24820@users.noreply.github.com>
2023-07-28 21:32:25 -0300

    Create Travel.js

---
+++ Travel.js
@@ -0,0 +1,41 @@
+function clicar(){
+    let clicar = document.getElementById("button");
+        clicar.style.backgroundColor = 'green'
+        clicar.style.color = 'white'
+}
+
+function sair(){
+    let sair = document.getElementById("button");
+        sair.style.backgroundColor = 'white'
+        sair.style.color = 'black'
+}
+
+function comprar_01(){
+    let a = document.getElementById("p_1")
+        a.style.backgroundColor = 'green'
+        a.style.color = "white"
+        a.innerText = "COMPRA REALIZADA"
+}
+
+function comprar_02(){
+    let b = document.getElementById("p_2")
+        b.style.backgroundColor = 'green'
+        b.style.color = "white"
+        b.innerText = "COMPRA REALIZADA"
+    
+}
+
+function comprar_03(){
+    let c = document.getElementById("p_3")
+        c.style.backgroundColor = 'green'
+        c.style.color = "white"
+        c.innerText = "COMPRA REALIZADA"  
+}
+
+function comprar_04(){
+    let d = document.getElementById("p_4")
+        d.style.backgroundColor = 'green'
+        d.style.color = "white"
+        d.innerText = "COMPRA REALIZADA"  
+}
+    
